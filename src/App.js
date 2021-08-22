fe1befc1ff014d7c2c7f713e0b863dd172ea0fb2
radu <radustanciu841@yahoo.ro>
2021-08-22 19:10:54 +0300

    Change App in class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from "react";
+import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import { Route, Switch } from "react-router-dom";
@@ -10,38 +10,31 @@ import Checkout from "./pages/Checkout/Checkout";
 import Category from "./pages/Category/Category";
 import { auth } from "./apis/firebase";
 
-function App() {
-  const [availableUser, setAvailableUser] = useState(null);
-  const unsubscribeAuth = null;
-  useEffect(()=>{
-    const unsubscribeAuth =auth.onAuthStateChanged((user)=>{
-        setAvailableUser(user);
-        console.log(user);
-    })
-  })
-  // // constructor() {
-  //   super();
-  //   this.state = {
-  //     // initial, userul nu este autentificat in baza de date
-  //     availableUser: null,
-  //   };
-  // }
-  // unsubscribeAuth = null;
-  // // se face reqeust pentru a se prelua datee despre user si a se actualiza state-ul acestuia
-  // componentDidMount() {
-  //   this.unsubscribeAuth = auth.onAuthStateChanged((user) => {
-  //     this.setState({ availableUser: user });
-  //     console.log(user);
-  //   });
-  // }
-  // // cand componneta s-a demontat, se inchide abonarea la baza de date
-  // componentWillUnmount() {
-  //   this.unsubscribeAuth();
-  // }
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // initial, userul nu este autentificat in baza de date
+      availableUser: null,
+    };
+  }
+  unsubscribeAuth = null;
+  // se face reqeust pentru a se prelua datee despre user si a se actualiza state-ul acestuia
+  componentDidMount() {
+    this.unsubscribeAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ availableUser: user });
+      console.log(user);
+    });
+  }
+  // cand componneta s-a demontat, se inchide abonarea la baza de date
+  componentWillUnmount() {
+    this.unsubscribeAuth();
+  }
+  render() {
     return (
       <div>
         {/* pasam la componenta header ca props starea initiala a user-ului */}
-        <Header availableUser={availableUser} />
+        <Header availableUser={this.state.availableUser} />
         {/* componeneta Header se va alfa pe fiecare pagina a aplicatiei */}
         <Switch>
           <Route exact path="/" component={HomePage} />
@@ -54,6 +47,6 @@ function App() {
       </div>
     );
   }
-
+}
 
 export default App;
